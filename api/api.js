a7af1cf4efa4a8d8e118a28d32c05a60f361f693
zhuyinghui <404993465@qq.com>
2019-04-28 14:58:04 +0800

    。

--- api/api.js
+++ api/api.js
@@ -15,4 +15,22 @@ router.use('/encyclopediaInfo',require('./encyclopediaInfo'));
 
 router.use('/adoptInfo',require('./adoptInfo'));
 
+//添加商品模块
+router.post('/upfile',function (req,res) {
+  var multiparty = require('multiparty');
+  var form = new multiparty.Form();
+  form.uploadDir = 'static/upfile';
+  form.parse(req, function (err, fields, file) {
+    if(err){
+      console.log(err)
+    }
+    console.log(fields);
+	console.log(file);
+	res.send('提交成功');
+  });
+});
+router.get('/upfile',function(req,res){
+	res.send('upfile的数据')
+})
+
 module.exports=router;
